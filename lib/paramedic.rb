2fb4e334ff632af1bf345d865581e2164f374a06
Nisanth Chunduru <nisanth074@gmail.com>
2021-06-19 02:24:25 +0530

    Add missing requires

---
+++ lib/paramedic.rb
@@ -0,0 +1,9 @@
+require "paramedic/core_extensions/hash"
+
+require "paramedic/version"
+
+require "paramedic/errors"
+require "paramedic/param"
+require "paramedic/params"
+require "paramedic/action_controller_additions"
+
